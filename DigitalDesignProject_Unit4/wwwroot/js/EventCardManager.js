0ba19f6dfc83926fdf9119844c8f513e70580fa4
Berlm <berlm.business@gmail.com>
2021-02-22 13:02:28 +0530

    Modal Added

--- DigitalDesignProject_Unit4/wwwroot/js/EventCardManager.js
+++ DigitalDesignProject_Unit4/wwwroot/js/EventCardManager.js
@@ -3,6 +3,7 @@
 // Called from Body's OnLoad
 function InitializeEvents(model) {
     events = JSON.parse(model);
+    InitializeEventModal();
 }
 
 function OnHoverEnter(element) {
@@ -25,5 +26,37 @@ function OnHoverLeave(element) {
             element.style.backgroundImage = 'url(' + event.imgDefault + ')';
             break;
         }
+    }
+}
+
+function OnClick(element) {
+    var id = element.id;
+    alert(id);
+    ShowModal();
+}
+
+var modal;
+var close;
+
+function InitializeEventModal() {
+    modal = document.getElementById("Modal");
+    close = document.getElementsByClassName("modal-close")[0];
+}
+
+// When the user clicks on the button, open the modal
+function ShowModal() {
+    alert('id');
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+close.onclick = function () {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
     }
 }
