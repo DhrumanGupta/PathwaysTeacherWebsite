488c249ac73de3955dcb84dc98f08e88f0f30207
Berlm <berlm.business@gmail.com>
2021-02-24 19:41:08 +0530

    Reverted to .NET Core

--- DigitalDesignProject_Unit4/Program.cs
+++ DigitalDesignProject_Unit4/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,7 +14,10 @@ namespace Website
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            CreateHostBuilder(args)
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .Build()
+                .Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
